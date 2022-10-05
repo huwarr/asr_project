cff3b7d06d0bb1775135a5378cfa6b581fd3b816
huwarr <mikhalmatova@gmail.com>
2022-10-05 12:07:52 +0300

    fix TimeStretch params (do we actually need it?)

--- hw_asr/augmentations/spectrogram_augmentations/TimeStretch.py
+++ hw_asr/augmentations/spectrogram_augmentations/TimeStretch.py
@@ -8,7 +8,7 @@ from hw_asr.augmentations.base import AugmentationBase
 class TimeStretch(AugmentationBase):
     def __init__(self, n_freq, **kwargs):
         self._aug = torchaudio.transforms.TimeStretch(n_freq=n_freq, **kwargs)
-        self.rates = [1.2, 0.8, 1.5, 1.]
+        self.rates = [1.2, 1.1, 1., 0.8, 0.9]
 
     def __call__(self, data: Tensor):
         return self._aug(data, random.choice(self.rates))
