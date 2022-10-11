35eaadbf7ad17b8d869380e525033a75c858cbac
huwarr <mikhalmatova@gmail.com>
2022-10-11 14:12:57 +0300

    РёРіРї Р°С€С‡

--- hw_asr/datasets/__init__.py
+++ hw_asr/datasets/__init__.py
@@ -1,13 +1,9 @@
 from hw_asr.datasets.custom_audio_dataset import CustomAudioDataset
 from hw_asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from hw_asr.datasets.librispeech_dataset import LibrispeechDataset
-from hw_asr.datasets.lj_dataset import LJDataset
-from hw_asr.datasets.common_voice_dataset import CommonVoiceDataset
 
 __all__ = [
     "LibrispeechDataset",
     "CustomDirAudioDataset",
-    "CustomAudioDataset",
-    "LJDataset",
-    "CommonVoiceDataset"
+    "CustomAudioDataset"
 ]
